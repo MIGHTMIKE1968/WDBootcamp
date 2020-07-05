ceb9bd7798e15199196bd668a41bc0c7163d0dd6
MLDODSON <mldodson@fullsail.edu>
2020-07-05 15:05:13 -0500

    JS Stalker Exercise

---
+++ JavaScript/stalk.js
@@ -0,0 +1,23 @@
+// JS Stalker Exercise
+
+let firstName = prompt("Enter your first name.");
+let lastName = prompt("Enter your last name.");
+console.log("My name is " + firstName + " " + lastName +".");
+
+let age = prompt("Enter your age.");
+let placeOfBirth = prompt("Enter your place of birth");
+console.log("I am " + age + " years old. I was born in " + placeOfBirth + ".");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
