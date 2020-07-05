7a4d0a2a916c7a7d57cfafa39cbd5ac3fad4b2d3
MLDODSON <mldodson@fullsail.edu>
2020-07-05 14:33:18 -0500

    JS Basics - primitive types, variables, alerts and prompts

---
+++ JavaScript/basics.js
@@ -0,0 +1,64 @@
+// Variables are containers that store values
+
+
+var autobotName = "Ultra Magnus";
+console.log(autobotName + " is a high ranking autobot warrior.");
+
+var leaderOne = "Optimus Prime";
+console.log(leaderOne + " is the leader of the noble Autobots.");
+
+var leaderTwo = "Megatron";
+console.log(leaderTwo + " is the cruel leader of the evil Decepticons.");
+
+console.log(leaderOne + " and " + leaderTwo + " are mortal enemies.");
+
+leaderTwo = "Shockwave";
+
+console.log(leaderTwo + " is now the new leader of the Decepticons.");
+
+var num = 52;
+var person = "Chuckie";
+var day = "Tuesday";
+
+console.log(person + " will turn " + num + " on " + day + ".");
+
+console.log(person.length);
+console.log(day[4]);
+console.log(num / 26);
+
+var city;
+console.log(city);
+
+var theVoid = "empty";
+theVoid = null;
+
+console.log(theVoid);
+
+var beautiful = true;
+
+console.log(beautiful);
+
+
+// Built in JS methods
+
+// Alert
+let friend = "Fred";
+alert("Wake up " + friend + "! Time to get up!");
+
+// Prompt
+let place = prompt("Where are you from?");
+console.log("I am from " + place + ".");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
