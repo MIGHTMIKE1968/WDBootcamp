3ae13b8c974fa5318130720f7bce06415cac4ad7
MLDODSON <mldodson@fullsail.edu>
2020-07-06 17:26:48 -0500

    JS Logical Operators

---
+++ JavaScript/basics2.js
@@ -0,0 +1,24 @@
+// Logical Operators
+
+var x = 10;
+var y = "a";
+
+y === "b" || x >= 10
+// Will evealuate to true because 1 side, x = 10 is true
+
+var a = 3;
+var b = 8;
+
+!(a == "3" || a === b) && !(b != 8 && a <= b)
+
+
+let c = 5
+let d = 7
+
+c > 2 && d >= 6
+
+d > 12 || c === 5
+
+!(d === 7)
+
+!(c >= 12) && d >= 4
