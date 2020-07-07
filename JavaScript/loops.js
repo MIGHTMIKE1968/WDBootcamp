022d14be3194325847c471dbd7ffebf42e211d9b
MLDODSON <mldodson@fullsail.edu>
2020-07-07 11:22:33 -0500

    More while loop practice

--- JavaScript/loops.js
+++ JavaScript/loops.js
@@ -1,5 +1,6 @@
 // While Loops
 
+/*
 let count = 1;
 
 while(count < 10) {
@@ -35,3 +36,46 @@ while(letter < name.length) {
     console.log(name[letter])
     letter++;
 }
+
+// While Loops Exercise 1
+
+let num = 1;
+
+while(num <= 12) {
+    console.log(num);
+    num += 2;
+}
+// Should print out 1, 3, 5, 7, 9, 11
+
+let nums = 1;
+
+while(nums <= 20) {
+    if(nums % 4 === 0) {
+        console.log(nums);
+    }
+    nums++;
+}
+// Sould print out all numbers divisible by 4
+
+let minus = 100;
+
+while(minus < 150) {
+    console.log(minus + 1);
+    minus--;
+}
+// Infinate Loop
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
