3053175345828debe1a2fd922ab47827104319c1
MLDODSON <mldodson@fullsail.edu>
2020-07-07 15:27:44 -0500

    JS Functions practice

---
+++ JavaScript/functions.js
@@ -0,0 +1,32 @@
+// Functions
+
+function sayHi() {
+    console.log("HOLA!");
+}
+
+sayHi();
+
+// Arrow function
+sayBye = () => {
+    console.log("ADIOS!");
+}
+
+sayBye();
+
+function singIt() {
+    console.log("R-U-B-B-E-R, F-A-N-S.");
+    console.log("Rubber Fans and Funkateers.");
+}
+
+singIt();
+singIt();
+
+singSong = () => {
+    console.log("Keep on with the force stop.");
+    console.log("Don't stop til you get enough.");
+}
+
+singSong();
+singSong();
+
+
