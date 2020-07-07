b9a2983b095290afabc3855d7f023535047efb6c
MLDODSON <mldodson@fullsail.edu>
2020-07-07 15:58:26 -0500

    JS Function arguments

--- JavaScript/functions.js
+++ JavaScript/functions.js
@@ -29,4 +29,55 @@ singSong = () => {
 singSong();
 singSong();
 
+// Arguements
+function siblings(a,b,c) {
+    console.log(a + ", " + b + ", and " + c + " are my brother and sisters.");
+}
+
+siblings("Kevin", "Angie", "Marie");
+
+square = (num) => {
+    console.log(num * num);
+}
+
+square(6);
+
+function add(a,b) {
+    console.log(a + b);
+}
+
+add(100, 35);
+
+greeting = (name) => {
+    console.log("Ah the name is " + name + " baby!");
+}
+
+greeting("Bootsy");
+greeting("Dr. Funkenstein");
+
+function modulo(x,y) {
+    console.log(x % y);
+}
+
+modulo(78, 15);
+
+
+amount = (z) => {
+    if(z < 10) {
+        console.log("You don't have enough money.");
+    } else {
+        console.log("You have enough cash to buy this.");
+    }
+    
+}
+
+amount(9);
+
+
+
+
+
+
+
+
 
