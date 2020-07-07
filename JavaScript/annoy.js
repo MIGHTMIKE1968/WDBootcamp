bbf7a88118273570c53b605659f6ac2195a46ecd
MLDODSON <mldodson@fullsail.edu>
2020-07-07 12:40:54 -0500

    Annoy O Matic while loop exercise

---
+++ JavaScript/annoy.js
@@ -0,0 +1,9 @@
+// Annoy O Matic
+
+let answer = prompt("Are we there yet Papa Smurf?");
+
+while(answer !== "yes" && answer !== "yeah") {
+    let answer = prompt("Are we there yet Papa Smurf?");
+}
+
+alert("YES, WE ARE HERE!! SHUT UP!!");
