4d373dce9f9e064a249da30a44fd6d744daaea08
MLDODSON <mldodson@fullsail.edu>
2020-07-07 11:03:39 -0500

    while loop practice

---
+++ JavaScript/loops.js
@@ -0,0 +1,37 @@
+// While Loops
+
+let count = 1;
+
+while(count < 10) {
+    console.log("Let the count begin: " + count);
+    count++;
+}
+
+let num = 5;
+
+while(num <= 15) {
+    console.log("Start counting:" + num);
+    num++;
+}
+
+let double = 2;
+
+while(double <= 10) {
+    console.log("Count in 2s: " + double);
+    double+=2;
+}
+
+let tres = 0;
+
+while(tres <= 15) {
+    console.log("Count in 3s " + tres);
+    tres+=3;
+}
+
+let name = "Bubba Ray Dudley";
+let letter = 0;
+
+while(letter < name.length) {
+    console.log(name[letter])
+    letter++;
+}
