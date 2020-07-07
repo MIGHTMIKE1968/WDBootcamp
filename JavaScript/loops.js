67b3ff4610f8ca8b22b6e85bb8f03f9adc66ff3f
MLDODSON <mldodson@fullsail.edu>
2020-07-07 12:10:20 -0500

    While loops problem set

--- JavaScript/loops.js
+++ JavaScript/loops.js
@@ -67,6 +67,49 @@ while(minus < 150) {
 */
 
 
+// **** WHILE LOOP PROBLEM SET ****
+
+// 1. Print all numbers between -10 and 19
+console.log("Print all numbers between -10 and 19.");
+let num = -10;
+
+while(num < 20) {
+    console.log(num);
+    num++;
+}
+
+// 2. Print all even numbers between 10 and 40
+console.log("Print all even numbers between 10 and 40.");
+let nums = 10;
+
+while(nums <= 40) {
+    console.log(nums);
+    nums+=2;
+}
+
+
+// 3. Print all odd numbers between 300 and 333
+console.log("Print all odd numbers between 300 and 333.");
+let count = 300;
+
+while(count <= 333) {
+    if(count % 2 === 1) {
+        console.log(count);    
+    }
+    count++;
+}
+
+// 4. Print all numbers divisible by 5 and 3 between 5 and 50
+console.log("Print all numbers divisible by 5 and 3 between 5 and 50.");
+let divide = 5;
+
+while(divide <= 50) {
+    if(divide % 3 === 0 && divide % 5 === 0) {
+        console.log(divide);    
+    }
+    divide++;
+}
+
 
 
 
