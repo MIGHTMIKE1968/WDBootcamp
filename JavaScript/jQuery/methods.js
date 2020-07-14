98dd4cb3206d7e4224f37589a61b7ca712272558
MLDODSON <mldodson@fullsail.edu>
2020-07-13 21:01:43 -0500

    jQuery Methods .text() and .html

---
+++ JavaScript/jQuery/methods.js
@@ -0,0 +1,17 @@
+// jQuery Methods
+//.text()
+
+$("h2").text("Text changed!!");
+
+$("h2").css("color", "red");
+
+let change = {
+    color: "purple",
+    textTransform: "uppercase"
+}
+
+//.html()
+$("h3").html("<h3>Rollin, Rollin, Rollin. Keep those doggies rollin!</h3>");
+$("h3").css(change);
+
+
