b5236d376bc604bede6413bcbcb103959cdd7dca
MLDODSON <mldodson@fullsail.edu>
2020-07-13 19:26:14 -0500

    Including jQuery and practice

---
+++ JavaScript/jQuery/jdemo.js
@@ -0,0 +1,25 @@
+let style1 = {
+    color: "darkblue",
+    backgroundColor: "yellow",
+    border: "1px solid purple",
+    textAlign: "center",
+    padding: "10px 0",
+    fontSize: "3rem",
+    height: "100%"
+}
+
+$("h1").css(style1)
+
+
+let ul = {
+    fontSize: "1.8rem"
+}
+
+$("ul").css(ul)
+
+let bod = {
+    width: "auto",
+    margin: "0 30px"
+}
+
+$("body").css(bod)
