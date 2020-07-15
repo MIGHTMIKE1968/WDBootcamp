1092335f8834fb4a600397a34bf34f3f81617a8c
MLDODSON <mldodson@fullsail.edu>
2020-07-14 22:06:50 -0500

    To-do List Project part 4

--- JavaScript/jQuery/ToDoProject/assets/js/todos.js
+++ JavaScript/jQuery/ToDoProject/assets/js/todos.js
@@ -1,4 +1,23 @@
 // Check Off Todos by Clicking
-$("li").click(function(){
+$("ul").on("click", "li", function(){
     $(this).toggleClass("completed");
 });
+
+// Click X to delete todo
+$("ul").on("click", "span", function(event){
+    $(this).parent().fadeOut(1000, function(){ 
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13) {
+        // Getting new todo text from input
+        let todoText = $(this).val();
+        $(this).val("");
+        // Create a new li and add to the ul
+        $("ul").append("<li><span>X</span> " + todoText + "</li")
+    }
+});
+
