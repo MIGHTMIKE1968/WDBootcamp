285629a8c163a204c42d335d44b0cd56b4df1712
MLDODSON <mldodson@fullsail.edu>
2020-07-17 19:21:02 -0500

    Node Route Params

--- node/FirstExpressApp/app.js
+++ node/FirstExpressApp/app.js
@@ -14,6 +14,20 @@ app.get("/boss", function(req, res) {
     console.log("Someone wants to see the boss.");
 });
 
+app.get("/r/:subredditName", function(req, res) {
+    let subreddit = req.params.subredditName;
+    res.send("WELCOME TO THE " + subreddit.toUpperCase() + " SUBREDDIT!");
+});
+
+app.get("/r/:subredditName/comments/:id/:title", function(req, res) {
+    console.log(req.params);
+    res.send("Welcome to the comments page.");
+}) 
+
+app.get("*", function(req, res) {
+    res.send("What do you think you're doing Dave?");
+});
+
 
 console.log("You are now listening live on port 3,000!");
 app.listen(3000);
