e8a09a5aff45a82aeabbc75949cf234da26a8b06
MLDODSON <mldodson@fullsail.edu>
2020-07-18 14:21:48 -0500

    EJS Conditionals and Loops

--- node/MoreExpress/ejs_demo/app.js
+++ node/MoreExpress/ejs_demo/app.js
@@ -10,6 +10,16 @@ app.get("/meet/:thing", function(req, res) {
   res.render('meet.ejs', {thingVar: thing});
 });
 
+app.get("/posts", function(req, res) {
+  let posts = [
+      {title: "My Pal Archie", author: "by Jughead Jones"},
+      {title: "Living With Venom", author: "by Bane"},
+      {title: "How To Be A Good Hero", author: "by The Tick"}
+  ];
+
+  res.render('posts.ejs', {posts: posts});
+});
+
 app.get("*", function(req, res) {
   res.send("Opps! You messed up!");
 })
