2eefc9b552f7dc329c3b6c9ad0116cbf37d9863f
MLDODSON <mldodson@fullsail.edu>
2020-07-20 11:22:10 -0500

    Yelp Camp, version 1: Styling the campgrounds

--- node/YelpCamp/V1/app.js
+++ node/YelpCamp/V1/app.js
@@ -6,9 +6,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 const campgrounds = [
-  {name: "Catfish Creek", image:"https://pixabay.com/get/57e0d74a4255ad14f1dc8460da293277103ddfe6505770_640.jpg"},
-  {name: "Busch's Wildlife", image:"https://pixabay.com/get/54e4d4424a54a514f1dc8460da293277103ddfe6515673_640.jpg"},
-  {name: "Chippewa Lake", image:"https://pixabay.com/get/57e6dd424e5aa914f1dc8460da293277103ddfe6575374_640.jpg"}
+  {name: "Catfish Lake", image:"https://images.unsplash.com/photo-1508873696983-2dfd5898f08b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"},
+  {name: "Busch's Wildlife", image:"https://images.unsplash.com/photo-1464207687429-7505649dae38?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1952&q=80"},
+  {name: "Chippewa Camping", image:"https://images.unsplash.com/photo-1501785888041-af3ef285b470?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"},
+  {name: "Peace Valley", image:"https://images.unsplash.com/photo-1516132006923-6cf348e5dee2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1568&q=80"}
 ];
 
 app.get("/", function(req, res) {
