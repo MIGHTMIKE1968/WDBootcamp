3b6ce8a24bbef2a74b31d768d96a924010bb54b6
MLDODSON <mldodson@fullsail.edu>
2020-07-22 17:42:25 -0500

    Blog App update: Creating the Edit and Update Routes

--- node/BlogApp/index.js
+++ node/BlogApp/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 
 // App Config
@@ -14,6 +15,7 @@ mongoose.connect('mongodb://localhost:27017/blog_app', {
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(methodOverride("_method"))
 
 // Mongoose/Model Config
 let blogSchema = new mongoose.Schema( {
@@ -84,6 +86,28 @@ app.get("/blogs/:id", function(req, res) {
           res.redirect("/blogs");
         } else {
           res.render("show", {blog: foundBlog});
+        }
+    });
+});
+
+// EDIT ROUTE
+app.get("/blogs/:id/edit", function(req, res) {
+  Blog.findById(req.params.id, function(err, foundBlog) {
+      if(err) {
+        res.redirect("/blogs");
+      } else {
+        res.render("edit", {blog: foundBlog});
+      }
+  });
+});
+
+// UPDATE ROUTE
+app.put("/blogs/:id", function(req, res) {
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog) {
+        if(err) {
+          res.redirect("/blogs");
+        } else {
+          res.redirect("/blogs/" + req.params.id);
         }
     });
 });
