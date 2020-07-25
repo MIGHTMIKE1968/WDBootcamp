ea23dff9ec85b57973ff8660a98b12d1d1a43635
MLDODSON <mldodson@fullsail.edu>
2020-07-24 19:00:56 -0500

    Secret Page App - Part 2

--- node/Authentication/AuthDemo/app.js
+++ node/Authentication/AuthDemo/app.js
@@ -1,6 +1,10 @@
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
+const express               = require('express'),
+      mongoose              = require('mongoose'),
+      passport              = require('passport'),
+      bodyParser            = require('body-parser'),
+      User                  = require('./models/user'),
+      LocalStrategy         = require('passport-local'),
+      passportLocalMongoose = require('passport-local-mongoose')
 
 mongoose.connect('mongodb://localhost:27017/auth_demo_app', {
   useNewUrlParser: true,
@@ -9,7 +13,20 @@ mongoose.connect('mongodb://localhost:27017/auth_demo_app', {
 .then(() => console.log('Connected to DB!'))
 .catch(error => console.log(error.message));
 
+const app = express();
 app.set("view engine", "ejs");
+
+app.use(require("express-session")( {
+    secret: "The Mothership Connection has landed ladies and gentlemen",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res) {
   res.render("home");
