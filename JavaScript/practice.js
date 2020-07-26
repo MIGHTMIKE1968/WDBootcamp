930ec3e3403f53b3911796a7ec6f4556bd707c5f
MLDODSON <mldodson@fullsail.edu>
2020-07-26 14:10:03 -0500

    Practice with the 'this' keyword

---
+++ JavaScript/practice.js
@@ -0,0 +1,56 @@
+// using 'this'
+
+let person = {
+    firstName: "Bruce",
+    lastName : "Thirdkill",
+    age      : 53,
+    fullName : function() {
+        return this.firstName + " " + this.lastName + " is " + this.age + ".";
+    }
+};
+
+console.log(person.fullName());
+
+
+let team = {
+    city : "St. Louis",
+    sport: "Baseball",
+    name : "Cardinals",
+    func : function() {
+        return "The " + this.city + " " + this.sport + " " + this.name + ".";
+    }
+}
+
+console.log(team.func());
+
+let decepticon = {
+    name   : "Megatron",
+    villian: function() {
+        return this.name;
+    }
+}
+
+console.log(decepticon.villian());
+
+let aunt = {
+    name : ["May", "Barbara", "Helen", "Alice"],
+    sayHi: "Good morning",
+    relative: function() {
+        return this.sayHi + " aunt " + this.name[3] + ".";
+    }
+}
+
+console.log(aunt.relative());
+
+
+
+
+
+
+
+
+
+
+
+
+
