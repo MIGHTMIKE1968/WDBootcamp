18122a6671803b4c117c6e7237ffdae65b472550
MLDODSON <mldodson@fullsail.edu>
2020-07-28 16:53:19 -0500

    YelpCamp-V10 Updates - Campground Authorization pt. 1

--- node/YelpCamp/V10/routes/campgrounds.js
+++ node/YelpCamp/V10/routes/campgrounds.js
@@ -58,14 +58,10 @@ router.get("/:id", function(req, res) {
 });
 
 // EDIT Campground Route
-router.get("/:id/edit", function(req, res) {
-  Campground.findById(req.params.id, function(err, foundCampground) {
-      if(err) {
-        res.redirect("/campgrounds");
-      } else {
-          res.render("campgrounds/edit", {campground: foundCampground});
-      }
-  });
+router.get("/:id/edit", checkCampgroundOwnership, function(req, res) {
+    Campground.findById(req.params.id, function(err, foundCampground) {
+        res.render("campgrounds/edit", {campground: foundCampground});
+    });
 });
 
 // UPDATE Campground Route
@@ -99,6 +95,26 @@ function isLoggedIn(req, res, next) {
         return next();
     }
     res.redirect("/login");
+}
+
+function checkCampgroundOwnership(req, res, next) {
+      if(req.isAuthenticated()) {
+          Campground.findById(req.params.id, function(err, foundCampground) {
+              if(err) {
+                res.redirect("back");
+              } else {
+                // does the user own the campground?
+              if(foundCampground.author.id.equals(req.user._id)) {
+                  next();
+              } else {
+                  res.redirect("back");
+              }
+            }
+        });
+      } else {
+          res.redirect("back");
+      }
+
 }
 
 module.exports = router;
