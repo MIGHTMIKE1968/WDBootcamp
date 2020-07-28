d8e1e47b964296d0662a9939f1809d600a1adf47
MLDODSON <mldodson@fullsail.edu>
2020-07-28 18:08:11 -0500

    YelpCamp-V10 Updates - Campground Authorization pt. 2

--- node/YelpCamp/V10/routes/campgrounds.js
+++ node/YelpCamp/V10/routes/campgrounds.js
@@ -65,7 +65,7 @@ router.get("/:id/edit", checkCampgroundOwnership, function(req, res) {
 });
 
 // UPDATE Campground Route
-router.put("/:id", function(req, res) {
+router.put("/:id", checkCampgroundOwnership, function(req, res) {
     // find and update the correct campground
     Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground) {
         if(err) {
@@ -78,7 +78,7 @@ router.put("/:id", function(req, res) {
 });
 
 // DESTROY Campground Route
-router.delete("/:id", function(req, res) {
+router.delete("/:id", checkCampgroundOwnership, function(req, res) {
   Campground.findByIdAndRemove(req.params.id, function(err) {
       if(err) {
         res.redirect("/campgrounds");
